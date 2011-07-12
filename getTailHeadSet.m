e81f16819c662d71d43061ceabff07c8e266f429
unknown <Aravi@.(none)>
2011-07-12 16:01:48 -0400

    ability to select frames to analyze

--- getTailHeadSet.m
+++ getTailHeadSet.m
@@ -1,9 +1,16 @@
-function [headPos,tailPos]=getTailHeadSet(video)
+function [headPos,tailPos]=getTailHeadSet(video,varargin)
 %asks the user to select the tail and the head for each frame
 
+
 vid=VideoReader(video);
-for k=1:vid.NumberOfFrames
-    disp(strcat(num2str(k), ' out of', num2str(vid.NumberOfFrames)));
+firstFrame=1;
+lastFrame=vid.NumberOfFrames;
+if(length(varargin)==2)
+    firstFrame=varargin{1}
+    lastFrame=varargin{2}
+end
+for k=firstFrame:lastFrame
+    disp(strcat(num2str(k), ' out of', num2str(lastFrame)));
     [headPos(k,:),tailPos(k,:)]=findTailHead(read(vid,k));
 end
 
--- getTurnSet.m
+++ getTurnSet.m
@@ -1,26 +1,9 @@
-function turnSet=getTurnSet(bodyVectors)
+function [dTheta]=getTurnSet(bodyVectors)
 %gets the reorientations of the worm
 
-axisVector=bodyVectors(:,1);
-%initialize reference vector
-dThetaPast=0;
-%initialize previous dTheta
-counter=0;
-%initialize reorientation counter
-index=1;    
-for k=1:size(bodyVectors,2)
-    dTheta=calculateAngle(bodyVectors(:,k),axisVector)
-    if(abs(dTheta-dThetaPast)<5)
-        counter=counter+1;
-    else
-         counter=0;
-    end
-    if(counter>3)
-        turnSet(:,:,index)=[k-40,k,dTheta]
-        axisVector=bodyVectors(:,k)
-        counter=0;
-        index=index+1;
-    end
-    dThetaPast=dTheta;
+for k=1:length(bodyVectors)-2
+    theta1=calculateAngle(bodyVectors(:,k),bodyVectors(:,k+1));
+    theta2=calculateAngle(bodyVectors(:,k+1),bodyVectors(:,k+2));
+    dTheta(k)=theta2-theta1;
 end
 end
